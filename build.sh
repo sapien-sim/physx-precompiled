e0ec90fc3d6adce9b56208cec68496e43a9f5eba
Fanbo Xiang <fxiang@eng.ucsd.edu>
2024-03-19 14:22:28 -0700

    precompile physx

---
+++ build.sh
@@ -0,0 +1,7 @@
+git clone -b 105.1-physx-5.3.1 --depth 1 --recursive https://github.com/NVIDIA-Omniverse/PhysX.git
+cp linux.xml Physx/physx/buildtools/presets/public/linux.xml
+sed -i 's/-Werror//g' PhysX/physx/source/compiler/cmake/linux/CMakeLists.txt
+cd PhysX/physx
+./generate_projects.sh linux
+cmake --build compiler/linux-release --config Release
+cmake --build compiler/linux-debug --config Debug
