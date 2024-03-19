faa8bc0658a4b972663942ca69111b0eae043a3d
Fanbo Xiang <fxiang@eng.ucsd.edu>
2024-03-19 14:53:35 -0700

    update

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,6 @@
 git clone -b $(cat branch) --depth 1 --recursive https://github.com/NVIDIA-Omniverse/PhysX.git
-cp linux.xml Physx/physx/buildtools/presets/public/linux.xml
+sed -i 's/name="PX_BUILDSNIPPETS" value="True"/name="PX_BUILDSNIPPETS" value="False"/g' PhysX/physx/buildtools/presets/public/linux.xml
+sed -i 's/name="PX_BUILDPVDRUNTIME" value="True"/name="PX_BUILDPVDRUNTIME" value="False"/g' PhysX/physx/buildtools/presets/public/linux.xml
 sed -i 's/-Werror//g' PhysX/physx/source/compiler/cmake/linux/CMakeLists.txt
 cd PhysX/physx
 ./generate_projects.sh linux
--- build-windows.sh
+++ build-windows.sh
@@ -1,5 +1,7 @@
 git clone -b $(cat branch) --depth 1 --recursive https://github.com/NVIDIA-Omniverse/PhysX.git
-cp vc17win64.xml Physx/physx/buildtools/presets/public/vc17win64.xml
+sed -i 's/name="PX_BUILDSNIPPETS" value="True"/name="PX_BUILDSNIPPETS" value="False"/g' PhysX/physx/buildtools/presets/public/vc17win64.xml
+sed -i 's/name="PX_BUILDPVDRUNTIME" value="True"/name="PX_BUILDPVDRUNTIME" value="False"/g' PhysX/physx/buildtools/presets/public/vc17win64.xml
+sed -i 's/name="PX_GENERATE_STATIC_LIBRARIES" value="False"/name="PX_GENERATE_STATIC_LIBRARIES" value="True"/g' PhysX/physx/buildtools/presets/public/vc17win64.xml
 cd PhysX/physx
 ./generate_projects.bat vc17win64
 cmake --build compiler/vc17win64 --config Release
