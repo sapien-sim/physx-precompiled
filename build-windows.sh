088814a38c9f1068b3a4e4a43ade1d4a17887864
Fanbo Xiang <xiangfanbo@gmail.com>
2024-04-19 00:44:44 -0700

    switch to our fork

--- build-windows.sh
+++ build-windows.sh
@@ -1,4 +1,4 @@
-git clone -b $(cat branch) --depth 1 --recursive https://github.com/NVIDIA-Omniverse/PhysX.git
+git clone -b $(cat branch) --depth 1 --recursive https://github.com/sapien-sim/PhysX.git
 sed -i 's/name="PX_BUILDSNIPPETS" value="True"/name="PX_BUILDSNIPPETS" value="False"/g' PhysX/physx/buildtools/presets/public/vc17win64.xml
 sed -i 's/name="PX_BUILDPVDRUNTIME" value="True"/name="PX_BUILDPVDRUNTIME" value="False"/g' PhysX/physx/buildtools/presets/public/vc17win64.xml
 sed -i 's/name="PX_GENERATE_STATIC_LIBRARIES" value="False"/name="PX_GENERATE_STATIC_LIBRARIES" value="True"/g' PhysX/physx/buildtools/presets/public/vc17win64.xml
